36a744f2e2c498ec05553428982f1ce2cea1888e
n0f3 <n0f3@outlook.com>
2020-06-09 18:43:45 -0400

    promified interface

--- index.android.js
+++ index.android.js
@@ -1,204 +1,276 @@
-import {
-  NativeModules,
-  DeviceEventEmitter
-} from 'react-native';
+import { NativeModules, DeviceEventEmitter } from "react-native";
 
 const samsungHealth = NativeModules.RNSamsungHealth;
 
 class RNSamsungHealth {
-  authorize( callback) {
-    samsungHealth.connect(
-      [samsungHealth.STEP_COUNT],
-      (msg) => { callback(msg, false); },
-      (res) => { callback(false, res); },
-    );
+  authorize() {
+    return new Promise((resolve, reject) => {
+      samsungHealth.connect(
+        [samsungHealth.STEP_COUNT],
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
   stop() {
     samsungHealth.disconnect();
   }
 
-  getDailyStepCount(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
+  getDailyStepCount({
+    startDate = new Date().setHours(0, 0, 0, 0),
+    endDate = new Date().valueOf(),
+  }) {
+    return new Promise((resolve, reject) => {
+      samsungHealth.readStepCount(
+        startDate,
+        endDate,
+        (msg) => {
+          callback(msg, false);
+        },
+        (res) => {
+          if (res.length > 0) {
+            var resData = res.map(function (dev) {
+              var obj = {};
+              obj.source = dev.source.name;
+              obj.data = this.buildDailySteps(dev.data);
+              obj.sourceDetail = dev.source;
+              return obj;
+            }, this);
 
-
-    samsungHealth.readStepCount(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-          if (res.length>0) {
-              var resData = res.map(function(dev) {
-                  var obj = {};
-                  obj.source = dev.source.name;
-                  obj.data = this.buildDailySteps(dev.data);
-                  obj.sourceDetail = dev.source;
-                  return obj;
-                }, this);
-
-
-              callback(false, resData);
+            callback(false, resData);
           } else {
-              callback("There is no any steps data for this period", false);
+            callback("There is no any steps data for this period", false);
           }
-      }
-    );
+        }
+      );
+    });
   }
 
-  getWeight(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
+  getWeight(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-    samsungHealth.readWeight(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readWeight(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
-  getSleep(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
+  getSleep(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-    samsungHealth.readSleep(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readSleep(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
+  getHeartRate(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-
-  getHeartRate(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
-
-    samsungHealth.readHeartRate(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readHeartRate(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
-  getBodyTemprature(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
+  getBodyTemprature(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-    samsungHealth.readBodyTemprature(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readBodyTemprature(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
+  getBloodPressure(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-
-  getBloodPressure(options, callback) {
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
-
-    samsungHealth.readBloodPressure(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readBloodPressure(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
-  getHeight(options, callback) {
+  getHeight(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
-
-    samsungHealth.readHeight(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
-  }
-  
-  getCholesterol(options, callback) {
-
-    let startDate = options.startDate != undefined ? options.startDate : (new Date()).setHours(0,0,0,0);
-    let endDate = options.endDate != undefined ? options.endDate : (new Date()).valueOf();
-
-    samsungHealth.readCholesterol(startDate, endDate,
-      (msg) => { callback(msg, false); },
-      (res) => {
-        callback(false, res);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      samsungHealth.readHeight(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
   }
 
+  getCholesterol(options) {
+    let startDate =
+      options.startDate != undefined
+        ? options.startDate
+        : new Date().setHours(0, 0, 0, 0);
+    let endDate =
+      options.endDate != undefined ? options.endDate : new Date().valueOf();
 
+    return new Promise((resolve, reject) => {
+      samsungHealth.readCholesterol(
+        startDate,
+        endDate,
+        (msg) => {
+          reject(msg);
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    });
+  }
 
   usubscribeListeners() {
     DeviceEventEmitter.removeAllListeners();
   }
 
-  buildDailySteps(data)
-  {
-      var results = [];
-      for(var step of data) {
-          var date = step.start_time !== undefined ? new Date(step.start_time) : new Date(step.day_time);
+  buildDailySteps(data) {
+    var results = [];
+    for (var step of data) {
+      var date =
+        step.start_time !== undefined
+          ? new Date(step.start_time)
+          : new Date(step.day_time);
 
-          var day = ("0" + date.getDate()).slice(-2);
-          var month = ("0" + (date.getMonth()+1)).slice(-2);
-          var year = date.getFullYear();
-          var dateFormatted = year + "-" + month + "-" + day;
-          results.push({steps:step.count, date:dateFormatted,  calorie:step.calorie, distance: step.distance})
-        
-      }
-      return results;
+      var day = ("0" + date.getDate()).slice(-2);
+      var month = ("0" + (date.getMonth() + 1)).slice(-2);
+      var year = date.getFullYear();
+      var dateFormatted = year + "-" + month + "-" + day;
+      results.push({
+        steps: step.count,
+        date: dateFormatted,
+        calorie: step.calorie,
+        distance: step.distance,
+      });
+    }
+    return results;
   }
 
-  mergeResult(res)
-  {
-      results = {}
-      for(var dev of res)
-      {
-          if (!(dev.sourceDetail.group in results)) {
-              results[dev.sourceDetail.group] = {
-                  source: dev.source,
-                  sourceDetail: { group: dev.sourceDetail.group },
-                  stepsDate: {}
-              };
-          }
-
-          let group = results[dev.sourceDetail.group];
-
-          for (var step of dev.steps) {
-              if (!(step.date in group.stepsDate)) {
-                  group.stepsDate[step.date] = 0;
-              }
-
-              group.stepsDate[step.date] += step.value;
-          }
+  mergeResult(res) {
+    results = {};
+    for (var dev of res) {
+      if (!(dev.sourceDetail.group in results)) {
+        results[dev.sourceDetail.group] = {
+          source: dev.source,
+          sourceDetail: { group: dev.sourceDetail.group },
+          stepsDate: {},
+        };
       }
 
-      results2 = [];
-      for(var index in results) {
-          let group = results[index];
-          var steps = [];
-          for(var date in group.stepsDate) {
-              steps.push({
-                date: date,
-                value: group.stepsDate[date]
-              });
-          }
-          group.steps = steps.sort((a,b) => a.date < b.date ? -1 : 1);
-          delete group.stepsDate;
+      let group = results[dev.sourceDetail.group];
 
-          results2.push(group);
+      for (var step of dev.steps) {
+        if (!(step.date in group.stepsDate)) {
+          group.stepsDate[step.date] = 0;
+        }
+
+        group.stepsDate[step.date] += step.value;
       }
+    }
 
-      return results2;
+    results2 = [];
+    for (var index in results) {
+      let group = results[index];
+      var steps = [];
+      for (var date in group.stepsDate) {
+        steps.push({
+          date: date,
+          value: group.stepsDate[date],
+        });
+      }
+      group.steps = steps.sort((a, b) => (a.date < b.date ? -1 : 1));
+      delete group.stepsDate;
+
+      results2.push(group);
+    }
+
+    return results2;
   }
 }
-
 
 export default new RNSamsungHealth();
